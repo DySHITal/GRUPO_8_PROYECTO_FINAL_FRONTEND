0b5bf0ffd2696ee03f3b5417784feae48c5769a6
Miranda Frias <mirifv.96@gmail.com>
2023-09-13 21:53:49 -0300

    carga de servidores
    previo a cambios de alfon

--- scripts/channels.js
+++ scripts/channels.js
@@ -5,7 +5,7 @@ const parrafo = document.getElementById('empty')
 var	overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-
+const sideBar = document.querySelector(".sidebar ul");
 let visible = false;
 
 exChannel.addEventListener('click', () => {
@@ -33,15 +33,15 @@ btnCerrarPopup.addEventListener('click', function(e){
 });
 
 //Cargar servidores
-function cargarServidoresExplorar() {
-    fetch('/cargar_servidores')  //NOTA: DEBO OBTENER ID de canal o con nombre de servidor es suficinte?
-                                 //asi como tambien el nombre del servidor,  src de la imagen
-        .then(response => response.json())
-        .then(data => {
-            // Maneja los datos de los servidores
-            const servidores = data.servidores;
+function cargarServidoresExplorar(servidor) {
+    // fetch('/cargar_servidores')  //NOTA: DEBO OBTENER ID de canal o con nombre de servidor es suficinte?
+    //                              //asi como tambien el nombre del servidor,  src de la imagen
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         // Maneja los datos de los servidores
+            // const servidores = data.servidores;
 
-            servidores.forEach(servidor => {
+            // servidores.forEach(servidor => {
                 const nombreServidor = servidor.nombre;
                 const direccionImagen = servidor.imagen;
                 const servidorElement = document.createElement('div');
@@ -53,31 +53,45 @@ function cargarServidoresExplorar() {
 
                 // Agrega el servidor al DOM, por ejemplo, a un contenedor div con id="server-list"
                 containerServidor.appendChild(servidorElement);
-            });
-        })
-        .catch(error => {
-            console.error('Error al cargar los servidores:', error);
-        });
+            // });
+        // })
+        // .catch(error => {
+        //     console.error('Error al cargar los servidores:', error);
+        // });
 }
 
-function cargarsidebar() {
-    fetch('/servidoresdelcliente') //Recibo los servidores a los que esta registrado el cliente
-        .then(response => response.json())
-        .then(data => {
-                // Maneja los datos de los servidores
-            const servidores = data.servidores;
-            servidores.forEach(servidor => {
-            const nombreServidor = servidor.nombre;
-            const direccionImagen = servidor.imagen;
-const servidorElement = document.createElement('div');
-const imagenServidor = document.createElement('img');
-servidorElement.className = 'servidor';
-servidorElement.setAttribute('data-nombre-servidor', nombreServidor);
-servidorElement.textContent = nombreServidor;
-imagenServidor.src=direccionImagen;
 
-// Agrega el servidor al DOM, por ejemplo, a un contenedor div con id="server-list"
-containerServidor.appendChild(servidorElement);
-});
-})
+//SE CARGAN SERVIDORES A SIDEBAR
+function agregarServidorAlSidebar(servidor) {
+    // Crea un elemento de servidor en formato HTML deseado
+    const servidorElement = document.createElement("li");
+    servidorElement.className = "tooltip";
+
+    // Crea la imagen del servidor
+    const imagenServidor = document.createElement("img");
+    imagenServidor.className = "icono hover";
+    imagenServidor.src = servidor.imagen; 
+
+    // Crea el elemento del tooltip
+    const tooltipElement = document.createElement("span");
+    tooltipElement.className = "tooltiptext";
+    tooltipElement.textContent = servidor.nombre; // Reemplaza con la descripción del servidor
+
+    // Agragamos al elemento de servidor
+    servidorElement.appendChild(imagenServidor);
+    servidorElement.appendChild(tooltipElement);
+    
+    sideBar.appendChild(servidorElement);
 }
+
+//DATOS DE PRUEBA
+// Datos ficticios de servidores (reemplaza esto con los datos reales de tu fetch)
+const servidor1 = { nombre: "Servidor 1", imagen: "../src/assets/images/canales/consola.png" };
+const servidor2 = { nombre: "Servidor 2", imagen: "../src/assets/images/canales/musica.png" };
+
+// Agrega los servidores al DOM utilizando la función
+agregarServidorAlSidebar(servidor1);
+agregarServidorAlSidebar(servidor2);
+
+cargarServidoresExplorar(servidor1);
+cargarServidoresExplorar(servidor2);
