a89f0893f8b07035df301b80fc7a0e22c7544b25
Alfredo Moreno <alfredomoreno@servtop.com.ar>
2023-09-18 20:51:21 -0300

    registro de usuario creado, se modifico nombres de imgs

--- scripts/channels.js
+++ scripts/channels.js
@@ -1,13 +1,16 @@
 const exChannel = document.getElementById('explore');
 const containerCanal = document.getElementById('container_canal');
 const parrafo = document.getElementById('empty');
-const addChannel = document.getElementById('add')
+const addChannel = document.getElementById('add');
+const addServer = document.getElementsByClassName('canal');
 const overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 window.addEventListener('load', function(){
     getAlias();
 })
+
+
 
 let visible = false;
 let servidoresCargados = false;
@@ -74,14 +77,20 @@ function cargarServidoresExplorar() {
                 const containerServidor = document.getElementById('container_canal');
     
                 servidores.forEach(servidor => {
-                const nombreServidor = servidor; // Nombre del servidor
-                const servidorElement = document.createElement('div');
-                servidorElement.className = 'canal';
-                const h5 = document.createElement('h5');
-                h5.textContent = nombreServidor;
-                servidorElement.appendChild(h5);
-                // Agrega el servidor al DOM, por ejemplo, a un contenedor div con id="container_canal"
-                containerServidor.appendChild(servidorElement);
+                    const nombreServidor = servidor; // Nombre del servidor
+                    const rutaImagen = `../src/assets/images/canales/${nombreServidor}.png`;
+                    const servidorElement = document.createElement('div');
+                    servidorElement.className = 'canal';
+                    servidorElement.id = nombreServidor
+                    const img = document.createElement('img');
+                    img.src = rutaImagen;
+                    img.style.width = '100px';
+                    const h5 = document.createElement('h5');
+                    h5.textContent = nombreServidor;
+                    servidorElement.appendChild(img)
+                    servidorElement.appendChild(h5);
+                    // Agrega el servidor al DOM, por ejemplo, a un contenedor div con id="container_canal"
+                    containerServidor.appendChild(servidorElement);
                 });
             });
         } else {
@@ -95,26 +104,39 @@ function cargarServidoresExplorar() {
     });
 }
 
+// addServer.addEventListener('click', () => {
+//     cargarsidebar()
+// })
 
+// function cargarsidebar() {
+//     fetch('http://127.0.0.1:5000/servidores_del_usuario', {
+//         method: 'GET',
+//         credentials: 'include'
+//     }) //Recibo los servidores a los que esta registrado el cliente
+//     .then(response => {
+//         if (response.status === 200){
+//             return response.json().then(data => {
+//                 const servidores = data.servidores;
+//                 const containerServidor = document.getElementById('user_servers');
+//                 const serverId = data.id
 
-function cargarsidebar() {
-    fetch('/servidoresdelcliente') //Recibo los servidores a los que esta registrado el cliente
-        .then(response => response.json())
-        .then(data => {
-                // Maneja los datos de los servidores
-            const servidores = data.servidores;
-            servidores.forEach(servidor => {
-            const nombreServidor = servidor.nombre;
-            const direccionImagen = servidor.imagen;
-            const servidorElement = document.createElement('div');
-            const imagenServidor = document.createElement('img');
-            servidorElement.className = 'servidor';
-            servidorElement.setAttribute('data-nombre-servidor', nombreServidor);
-            servidorElement.textContent = nombreServidor;
-            imagenServidor.src=direccionImagen;
-
-// Agrega el servidor al DOM, por ejemplo, a un contenedor div con id="server-list"
-            containerServidor.appendChild(servidorElement);
-                                            });
-                        })
-}
+//                 servidores.forEach(servidor => {
+//                     const rutaImagen = `../src/assets/images/canales/${nombreServidor}.png`;
+//                     const servidorElement = document.createElement('div');
+//                     const imagenServidor = document.createElement('img');
+//                     servidorElement.className = 'servidor';
+//                     servidorElement.setAttribute('data-nombre-servidor', nombreServidor);
+//                     imagenServidor.src = rutaImagen
+//                     containerServidor.appendChild(servidorElement);
+//                 });
+//             });
+//         } else {
+//             return response.json().then(data => {
+//                 document.getElementById('message').innerHTML = data.msg;
+//             });
+//         }
+//     })
+//     .catch(error => {
+//         document.getElementById('message').innerHTML = 'An error occurred.';
+//     });
+// }
