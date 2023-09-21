cf9f1e806281af4882316fe52c9863301e8fa649
Alfredo Moreno <alfredomoreno@servtop.com.ar>
2023-09-21 20:05:30 -0300

    Ahora funciona el cargar servidores al sidebar, y se implemento la forma para la creacion de servidores

--- scripts/channels.js
+++ scripts/channels.js
@@ -1,11 +1,11 @@
-const exChannel = document.getElementById('explore');
-const containerCanal = document.getElementById('container_canal');
-const parrafo = document.getElementById('empty');
-const addChannel = document.getElementById('add');
-const addServer = document.getElementsByClassName('canal');
-const overlay = document.getElementById('overlay'),
+const exChannel = document.getElementById('explore'),
+    containerCanal = document.getElementById('container_canal'),
+    parrafo = document.getElementById('empty'),
+    addChannel = document.getElementById('add'),
+    addServer = document.getElementsByClassName('canal'),
+    overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup')
 window.addEventListener('load', function(){
     getAlias();
     agregarServidorAlSidebar();
@@ -64,42 +64,42 @@ btnCerrarPopup.addEventListener('click', function(e){
 	popup.classList.remove('active');
 });
 
-
-// //funcion crear servidores
-// function crearServidor() {
-//     const data = {
-//         server_name: document.getElementById("server_name").value,
-//         server_descripcion: document.getElementById("server_descripcion").value,
-//     };
-
-//     fetch("http://127.0.0.1:5000/crear_server", {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(data),
-//         credentials: 'include'
-//     })
-//     .then(response => {
-//         if (response.status === 200) {
-//             return response.json().then(data => {
-//                 window.location.href = "vistaprincipal.html";
-//             });
-//         } else {
-//             return response.json().then(data => {
-//                 document.getElementById("message").innerHTML = data.msg;
-//             });
-//         }
-//     })
-//     .catch(error => {
-//         document.getElementById("message").innerHTML = "An error occurred.";
-//     });
-// }
-
 const crearServer = document.getElementById('crearServer')
-crearServer.addEventListener('click', function(){
-       crearServidor()
+crearServer.addEventListener('submit', function(e){
+    e.preventDefault()
+    crearServidor()
 })
+
+//funcion crear servidores
+function crearServidor() {
+    const data = {
+        server_name: document.getElementById("server_name").value,
+        server_descripcion: document.getElementById("server_description").value,
+    };
+    fetch("http://127.0.0.1:5000/crear_server", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        credentials: 'include'
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json().then(data => {
+                window.location.href = "vistaprincipal.html";
+            });
+        } else {
+            return response.json().then(data => {
+                document.getElementById("message").innerHTML = data.msg;
+            });
+        }
+    })
+    .catch(error => {
+        document.getElementById("message").innerHTML = "An error occurred.";
+    });
+}
+
 
 //Cargar servidores
 function cargarServidoresExplorar() {
@@ -142,44 +142,8 @@ function cargarServidoresExplorar() {
     });
 }
 
-// addServer.addEventListener('click', () => {
-//     cargarsidebar()
-// })
-
-// function cargarsidebar() {
-//     fetch('http://127.0.0.1:5000/servidores_del_usuario', {
-//         method: 'GET',
-//         credentials: 'include'
-//     }) //Recibo los servidores a los que esta registrado el cliente
-//     .then(response => {
-//         if (response.status === 200){
-//             return response.json().then(data => {
-//                 const servidores = data.servidores;
-//                 const containerServidor = document.getElementById('user_servers');
-//                 const serverId = data.id
-
-//                 servidores.forEach(servidor => {
-//                     const rutaImagen = `../src/assets/images/canales/${nombreServidor}.png`;
-//                     const servidorElement = document.createElement('div');
-//                     const imagenServidor = document.createElement('img');
-//                     servidorElement.className = 'servidor';
-//                     servidorElement.setAttribute('data-nombre-servidor', nombreServidor);
-//                     imagenServidor.src = rutaImagen
-//                     containerServidor.appendChild(servidorElement);
-//                 });
-//             });
-//         } else {
-//             return response.json().then(data => {
-//                 document.getElementById('message').innerHTML = data.msg;
-//             });
-//         }
-//     })
-//     .catch(error => {
-//         document.getElementById('message').innerHTML = 'An error occurred.';
-//     });
-// }
 //SE CARGAN SERVIDORES A SIDEBAR
-function agregarServidorAlSidebar(servidor) {
+function agregarServidorAlSidebar() {
     fetch('http://127.0.0.1:5000/servidores_del_usuario', {
         method: 'GET',
         credentials: 'include'
@@ -188,31 +152,31 @@ function agregarServidorAlSidebar(servidor) {
         if (response.status === 200) {
             return response.json().then(data => {
                 // Maneja los datos de los servidores
-                const servidores = data;
-    
-             servidores.forEach(servidor => {
-    
-            // Crea un elemento de servidor en formato li
-              const servidorElement = document.createElement("li");
-              servidorElement.className = "tooltip";
-              servidorElement.id = servidor.id;
+                const servidores = data.nombre_servidor;
+                const sideBar = document.getElementById('sideBar');
+                servidores.forEach(servidor => {
+        
+                // Crea un elemento de servidor en formato li
+                const servidorElement = document.createElement("li");
+                servidorElement.className = "tooltip";
+                servidorElement.id = servidor;
 
-            // Crea la imagen del servidor
-              const imagenServidor = document.createElement("img");
-              imagenServidor.className = "icono hover";
-              imagenServidor.src = servidor.imagen; 
+                // Crea la imagen del servidor
+                const imagenServidor = document.createElement("img");
+                imagenServidor.className = "icono hover";
+                imagenServidor.src = `../src/assets/images/canales/${servidor}.png`; 
 
-           // Crea el elemento del tooltip
-              const tooltipElement = document.createElement("span");
-              tooltipElement.className = "tooltiptext";
-              tooltipElement.textContent = servidor.nombre; 
+            // Crea el elemento del tooltip
+                const tooltipElement = document.createElement("span");
+                tooltipElement.className = "tooltiptext";
+                tooltipElement.textContent = servidor; 
 
-           // Agragamos al elemento de servidor
-              servidorElement.appendChild(imagenServidor);
-             servidorElement.appendChild(tooltipElement);
-    
-             sideBar.appendChild(servidorElement);
-            });
+            // Agragamos al elemento de servidor
+                servidorElement.appendChild(imagenServidor);
+                servidorElement.appendChild(tooltipElement);
+        
+                sideBar.appendChild(servidorElement);
+                });
         });
         } else {
             return response.json().then(data => {
@@ -224,18 +188,18 @@ function agregarServidorAlSidebar(servidor) {
         document.getElementById('message').innerHTML = 'An error occurred.';
         });
         }; 
-    //FUNCION PARA MANIPULAR LOS CICKS EN LAS LISTAS
-function manejarClicEnLista(event) {
-    const elementoClicado = event.target;
-    const id = elementoClicado.id
+//     //FUNCION PARA MANIPULAR LOS CICKS EN LAS LISTAS
+// function manejarClicEnLista(event) {
+//     const elementoClicado = event.target;
+//     const id = elementoClicado.id
     
-    return id
-}
+//     return id
+// }
 
-// const lista = document.getElementById("miLista");
-// lista.addEventListener("click", manejarClicEnLista);
+// // const lista = document.getElementById("miLista");
+// // lista.addEventListener("click", manejarClicEnLista);
 
-    //SE CARGAN LOS CANALES DE UN SERVIDOR AL HACER CLICK EN UN SERVIDOR sideBar
-sideBar.addEventListener("click", manejarClicEnLista);
-    //SE CARGAN LOS MENSAJES AL HACER CLICK EN UN CANAL
-const msgs = document.querySelector(".msgs ul");
+//     //SE CARGAN LOS CANALES DE UN SERVIDOR AL HACER CLICK EN UN SERVIDOR sideBar
+// sideBar.addEventListener("click", manejarClicEnLista);
+//     //SE CARGAN LOS MENSAJES AL HACER CLICK EN UN CANAL
+// const msgs = document.querySelector(".msgs ul");
