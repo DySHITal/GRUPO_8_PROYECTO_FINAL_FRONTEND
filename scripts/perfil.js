78f6fb89d254711aa31a082463b48cc377fc618a
Miranda Frias <mirifv.96@gmail.com>
2023-09-28 00:02:54 -0300

    mostrar perfil

--- scripts/perfil.js
+++ scripts/perfil.js
@@ -1,41 +1,40 @@
-const infoUsuario = document.getElementsByClassName('info-usuario'),
-      servidoresCreados = document.getElementsByClassName('servidoresCreados'),
-      mensajeUsuario = document.getElementsByClassName('mensajeUsuario')
+const infoUsuario = document.querySelector('.infousuario')
+      servidoresCreados = document.querySelector('.servidoresCreados'),
+      mensajeUsuario = document.querySelector('mensajeUsuario')
 
 
 //CARGAMOS LA INFORMACION AL ACCEDER  A LA PAGINA
 document.addEventListener('DOMContentLoaded', function () {
     // Solicitud para obtener la información del usuario
     fetch('http://127.0.0.1:5000/ruta_info_usuario', {
-        method: 'GET',
-        credentials: 'include'
+    method: 'GET',
+    credentials: 'include'
+})
+    .then(response => {
+        if (response.status === 200) {
+            return response.json().then(data => {
+                
+                const infoUsuarioHTML = `
+                    <p><strong>Nombre:</strong> ${data.nombre || 'N/A'}</p>
+                    <p><strong>Apellido:</strong> ${data.apellido || 'N/A'}</p>
+                    <p><strong>Alias:</strong> ${data.alias || 'N/A'}</p>
+                    <p><strong>Correo:</strong> ${data.correo || 'N/A'}</p>
+                    <p><strong>Fecha Nacimiento:</strong> ${data.fechas_nacimiento || 'N/A'}</p>
+                `;
+
+                infoUsuario.innerHTML = infoUsuarioHTML;
+            });
+        } else {
+            return response.json().then(data => {
+                document.getElementById('message').innerHTML = data.msg;
+            });
+        }
     })
-        .then(response => {
-            if(response.status==200){
-                return response.json().then(data =>{
-                    console.log(data)
-            // Rellena la información del usuario en el elemento HTML
-            infoUsuario.innerHTML = `
-                <p><strong>Nombre:</strong> ${data.nombre}</p>
-                <p><strong>Apellido:</strong> ${data.apellido}</p>
-                <p><strong>Alias:</strong> ${data.alias}</p>
-                <p><strong>Correo:</strong> ${data.correo}</p>
-                <p><strong>Fecha Nacimiento:</strong> ${data.fechas_nacimiento}</p>
-            `;
-        });
-    
-      } else {
-          return response.json().then(data => {
-           document.getElementById('message').innerHTML = data.msg;
-        });
-      }})
+    .catch(error => {
+        console.error('Error al obtener la información del usuario', error);
+        document.getElementById('message').innerHTML = 'An error occurred.';
+    });
 
-        .catch(error => {
-            console.error('Error al obtener la información del usuario', error);
-            document.getElementById('message').innerHTML = 'An error occurred.';
-        });
-
-    // Realiza una solicitud Fetch para obtener los servidores creados desde Flask
     // Cargar servidores creados por el usuario
 const servidoresCreados = document.querySelector('.servidoresCreados');
 
@@ -85,11 +84,12 @@ fetch('http://127.0.0.1:5000/ruta_mensajes_enviados', {
         return response.json().then(data => {
             // Rellena la lista de mensajes enviados por el usuario
             mensajesUsuario.innerHTML = '';
+            console.log(data)
             data.mensajes.forEach(mensaje => {
                 const msj = document.createElement('li');
                 msj.textContent = mensaje.contenido;
                 msj.id=mensaje.id;
-                mensajesUsuario.appendChild(li);
+                mensajesUsuario.appendChild(msj);
 
                 // Agregar evento de clic solo si aún no se ha agregado
                 if (!msj.hasEventListeners) {
